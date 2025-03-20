2aa461abf41dd6672ac68bdd643fc0b0b2cab4ae
mohd kaif <mk2818356@gmail.com>
2025-03-21 00:17:21 +0530

    added db and http

---
+++ apps/http/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { prismaClient } from "store/client"
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/user",async function(req,res){
+    const { username, password } = req.body
+
+   try{
+        const response = await prismaClient.user.create({
+            data:{
+                username,
+                password
+            }
+        })
+        res.json({
+            id:response.id
+        })
+   }catch(e){
+        res.send("something happen wrong with db")
+   }
+
+})
+
+app.listen(8000,()=>{
+    console.log("server is running on port 8000")
+})
